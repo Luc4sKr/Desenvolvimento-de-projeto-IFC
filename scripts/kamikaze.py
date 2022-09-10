e70f5701219597b4fab64766ab7087b773b754bd
Lucas <lucaskreuch77@gmail.com>
2022-09-10 19:45:04 -0300

    Refatoração kamikaze

--- scripts/kamikaze.py
+++ scripts/kamikaze.py
@@ -5,20 +5,20 @@ class Kamikaze(pygame.sprite.Sprite):
     def __init__(self, x, shield, animation_list):
         pygame.sprite.Sprite.__init__(self)
 
-        self.__frame_index = 0  # Frame de animação
-        self.__update_time = pygame.time.get_ticks()
-        self.__animation_list = animation_list  # Lista de animação
-
-        self.__shield = shield  # É a "vida" do Enemy
-        self.__damage = KAMIKAZE_DAMAGE  # Dano
+        self.__frame_index = 0                        # Frame de animação
+        self.__update_time = pygame.time.get_ticks()  # Ajuda no update de animação
+        self.__animation_list = animation_list        # Lista de animação
+        self.__shield = shield                        # Shield
+        self.__damage = KAMIKAZE_DAMAGE               # Dano
 
         # Imagem
         self.__image = self.__animation_list[self.__frame_index]
         self.__image = pygame.transform.scale(self.__image, (ENEMY_SIZE_X, ENEMY_SIZE_Y))
         self.__rect = self.__image.get_rect()
 
-        self.__rect.center = (x, -20)
+        self.__rect.center = (x, -20)  # Posição onde o inimigo vai surgir
 
+        # Define a rotação do kamikaze dependendo de qual lado da tela ele está
         if self.__rect.x > SCREEN_X / 2:
             self.__image = pygame.transform.rotate(self.__image, 340)
         if self.__rect.x < SCREEN_X / 2:
@@ -75,3 +75,4 @@ class Kamikaze(pygame.sprite.Sprite):
     def damage(self, damage):
         if damage <= 0:
             self.__damage = 10
+            
